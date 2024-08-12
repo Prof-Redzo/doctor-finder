6aa1ac6bdd78f8bdeb34c9c30d6e88745b298c08
Redžo <154461862+Prof-Redzo@users.noreply.github.com>
2024-08-12 19:02:30 +0200

    created route for all services

--- routes/service-routes.js
+++ routes/service-routes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { createService } from "../controllers/service-controller.js";
+import { getService } from "../controllers/service-controller.js";
 import authorizeRole from "../middlewares/role-middleware.js";
 import authenticate from "../middlewares/auth-middleware.js";
 
 const router = express.Router();
 
 router.post("/services", authenticate, authorizeRole, createService);
+router.get("/services", getService);
 
 export default router;
